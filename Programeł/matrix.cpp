41ffff6e267d86903ee59f8adbd31bfe1e6e5c8a
domo233 <dgol.inpg@gmail.com>
2017-05-21 22:09:40 +0200

    Raport 20.05.2017 Konstruktor kopiujący, operator przypisania

--- Programeł/matrix.cpp
+++ Programeł/matrix.cpp
@@ -2,3 +2,38 @@
 #include "matrix.h"
 
 //metody
+
+//konstruktor kopiuj¹cy
+
+Matrix::Matrix(const Matrix & obiekt)
+{
+    col=obiekt.col;
+    row=obiekt.row;
+    wiersze = new Wektor*[row];
+    for(int i=0; i<(int)row; i++)
+    {
+        wiersze[i] = new Wektor(col);
+        *wiersze[i] = *obiekt.wiersze[i];
+    }
+}
+
+//operator przypisania
+
+Matrix & Matrix::operator=(const Matrix & obiekt)
+{
+    if(&obiekt==this) return *this;
+    for(int i=0; i<row; i++)
+    {
+        delete wiersze[i];
+    }
+    delete [] wiersze;
+    row=obiekt.row;
+    col=obiekt.col;
+    wiersze=new Wektor*[row];
+    for(int i=0; i<(int)row; i++)
+    {
+        wiersze[i] = new Wektor(col);
+        *wiersze[i] = *obiekt.wiersze[i];
+    }
+    return *this;
+}
